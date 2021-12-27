8298d76524fa7a9bbf5fb13bc8d36e8c902744a4
unknown <jlgriffith@protonmail.com>
2021-12-27 17:30:20 -0500

    Added logic to determine whether a water plot is close to land or not, and therefore whether it should be coastal.

--- convert_topographic_map.py
+++ convert_topographic_map.py
@@ -3,12 +3,12 @@ from PIL import Image
 from collections import OrderedDict
 import os
 
-input_file = './topographic_map.jpg'
-rgb_file = './output/rgb_values.csv'
-topography_file = './output/topographic_plots.csv'
-map_file = './output/map_plot_data.txt'
+inputFile = './topographic_map.jpg'
+rgbFile = './output/rgb_values.csv'
+plotDataFile = './output/map_plot_data.txt'
+coastDistanceFromLand = 4
 
-image = Image.open(input_file) 
+image = Image.open(inputFile) 
  
 # load the pixel info
 pixels = image.load()
@@ -25,7 +25,7 @@ for x in range(width):
       b = pixels[x,y][2]
       if (b > 210):
         plotType=3 # ocean
-        terrainType='TERRAIN_COAST'
+        terrainType='TERRAIN_OCEAN'
       elif (r > 210 and g < 160):
         plotType=0 # mountain
         terrainType='TERRAIN_GRASS'
@@ -37,25 +37,36 @@ for x in range(width):
         terrainType='TERRAIN_GRASS'
       key = str(x) + '|' + str(height-y-1)
       tiles[key] = {'x': x, 'y': height-y-1, 'r': r, 'g': g, 'b': b, 'plot': plotType, 'terrain': terrainType}
+
+# Determine whether terrain should be ocean or coast by its proximity to land plots
+for key, value in tiles.items():
+  if (value['plot'] == 3):
+    x = value['x']
+    y = value['y']
+    for adjacentX in list(range(-coastDistanceFromLand, coastDistanceFromLand)):
+      for adjacentY in [-coastDistanceFromLand, coastDistanceFromLand]:
+        key = str(x + adjacentX) + '|' + str(y + adjacentY)
+        if (key in tiles):
+          adjacentTile = tiles[key]
+          if (adjacentTile['plot'] == 0 or adjacentTile['plot'] == 1 or adjacentTile['plot'] == 2):
+            adjacentTile['terrain'] = 'TERRAIN_COAST'
  
 # save each pixel's RGB data to a file
-if (os.path.exists(rgb_file) and os.path.isfile(rgb_file)):
-  os.remove(rgb_file)
-  print(rgb_file + ' file deleted')
-with open(rgb_file, 'w+') as file: 
+if (os.path.exists(rgbFile) and os.path.isfile(rgbFile)):
+  os.remove(rgbFile)
+  print(rgbFile + ' file deleted')
+with open(rgbFile, 'w+') as file: 
   file.write('X,Y,R,G,B\n') 
-  for key, value in tiles.items():
-    tile = value
+  for key, tile in tiles.items():
     file.write('{0},{1},{2},{3},{4}\n'.format(tile['x'],tile['y'],tile['r'],tile['g'],tile['b']))
-print('Wrote file\'s RGG values to ' + rgb_file)
+print('Wrote file\'s RGG values to ' + rgbFile)
 
 # save each pixel's terrain data to a file
-if (os.path.exists(map_file) and os.path.isfile(map_file)):
-  os.remove(map_file)
-  print(map_file + ' file deleted')
-with open(map_file, 'w+') as file: 
+if (os.path.exists(plotDataFile) and os.path.isfile(plotDataFile)):
+  os.remove(plotDataFile)
+  print(plotDataFile + ' file deleted')
+with open(plotDataFile, 'w+') as file: 
   file.write('### Plot Info ###')
-  for key, value in tiles.items():
-    tile = value
+  for key, tile in tiles.items():
     file.write('\nBeginPlot\n\tx={0},y={1}\n\tTerrainType={2}\n\tPlotType={3}\nEndPlot'.format(tile['x'],tile['y'],tile['terrain'], tile['plot']))
-print('Created map file with plot-types: ' + map_file)
+print('Created map file with plot-types: ' + plotDataFile)
